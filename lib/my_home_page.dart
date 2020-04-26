f947b4edb7fffc34e90b25f282d091189b7e2848
Mahdi <mahdi.a.200066@gmail.com>
2020-04-26 20:44:59 +0430

    Initial commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Champion stage'),
+          centerTitle: true,
+        ),
+        body: Container(),
+      );
+}
